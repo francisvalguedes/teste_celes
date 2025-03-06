5a0897283a9ce59b79b7b42abc0ec7727096ac1f
francisvalguedes <francisval.guedes.soares.037@ufrn.edu.br>
2025-03-06 09:07:06 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+"""
+Creator: Francisval Guedes Soares
+Date: 2021
+"""
+
+import streamlit as st
+import pandas as pd
+
+import requests
+from io import StringIO
+
+
+def load_original_data(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return pd.read_csv(StringIO(response.text))
+    else:
+        st.error("Failed to load data from Celestrak.")
+        return None
+    
+
+norad_id = norad_id = st.number_input('Unique NORAD_CAT_ID', 0, 999999,value= 25544, format="%d")
+
+    
+urlCelestrak = 'https://celestrak.org/NORAD/elements/gp.php?CATNR='+ str(norad_id) +'&FORMAT=csv'
+
+
+st.write('spacetrak')
+elem_df = load_original_data(urlCelestrak)
+# elem_df = pd.read_csv(urlCelestrak)
+st.dataframe(elem_df)
