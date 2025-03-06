7fb88363c326fbbb86cb2be3ef0edf7fc68d1d81
francisvalguedes <francisval.guedes.soares.037@ufrn.edu.br>
2025-03-06 09:19:57 -0300

    up

--- main.py
+++ main.py
@@ -18,14 +18,15 @@ def load_original_data(url):
         st.error("Failed to load data from Celestrak.")
         return None
     
+def main():
+    norad_id = norad_id = st.number_input('Unique NORAD_CAT_ID', 0, 999999,value= 25544, format="%d")        
+    urlCelestrak = 'https://celestrak.org/NORAD/elements/gp.php?CATNR='+ str(norad_id) +'&FORMAT=csv'
 
-norad_id = norad_id = st.number_input('Unique NORAD_CAT_ID', 0, 999999,value= 25544, format="%d")
+    st.write('spacetrak')
+    elem_df = load_original_data(urlCelestrak)
+    # elem_df = pd.read_csv(urlCelestrak)
+    st.dataframe(elem_df)
 
-    
-urlCelestrak = 'https://celestrak.org/NORAD/elements/gp.php?CATNR='+ str(norad_id) +'&FORMAT=csv'
 
-
-st.write('spacetrak')
-elem_df = load_original_data(urlCelestrak)
-# elem_df = pd.read_csv(urlCelestrak)
-st.dataframe(elem_df)
+if __name__== '__main__':
+    main()
